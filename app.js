e7fe61a4581f405aab7233429c2f500929efb7eb
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-05-26 16:00:33 +0300

    fixed

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-const logger = require("morgan");
 
 const app = express();
 
 const producstRouter = require("./routes/products");
 const orderRouter = require("./routes/orders");
 
-// const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-// app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
