b94a2c443c8c0624bdaa195cb10d3cba6e678111
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2023-05-31 16:13:11 +0300

    move DB_HOST to process.env

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Nastia:HmHhSr62gx7NZ8SK@cluster0.zvbuz2e.mongodb.net/shops_products?retryWrites=true&w=majority";
-
+const { DB_HOST } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
